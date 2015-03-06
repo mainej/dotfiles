ae016fd80d2dc63cb7fae642675c745111c3bd38
Sheel Choksi <sheelchoksi@gmail.com>
2015-03-05 20:30:37 -0800

    Adding emacs aliases

--- emacs/link-emacs.sh
+++ emacs/link-emacs.sh
@@ -3,3 +3,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -f ~/.emacs
 mkdir -p ~/.emacs.d
 ln -s "${DIR}/emacs.d/init.el" ~/.emacs.d/init.el
+
+written_emacs_alias=`grep emacsclient ~/.bash_profile`
+if [ $? -ne 0 ]; then
+    echo "alias e='emacsclient -n -c -a \"\"'" >> ~/.bash_profile
+    echo "alias e='emacsclient -nw -a \"\"'" >> ~/.bash_profile
+fi
