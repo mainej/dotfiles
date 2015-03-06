de531f8e1ab6e0a56a654e2d86be161e7fdbedfa
Sheel Choksi <sheelchoksi@gmail.com>
2015-03-05 20:33:40 -0800

    Fix duplicate alias

--- emacs/link-emacs.sh
+++ emacs/link-emacs.sh
@@ -7,5 +7,5 @@ ln -s "${DIR}/emacs.d/init.el" ~/.emacs.d/init.el
 written_emacs_alias=`grep emacsclient ~/.bash_profile`
 if [ $? -ne 0 ]; then
     echo "alias e='emacsclient -n -c -a \"\"'" >> ~/.bash_profile
-    echo "alias e='emacsclient -nw -a \"\"'" >> ~/.bash_profile
+    echo "alias et='emacsclient -nw -a \"\"'" >> ~/.bash_profile
 fi
