f86439a2f770137bba6ef53baeb23245b5ea10f5
Sheel Choksi <sheelchoksi@gmail.com>
2015-03-08 15:21:23 -0700

    Auto refreshing buffer seemingly makes thing move
    It is distracting

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -228,10 +228,7 @@ the mode-line."
       mac-command-modifier 'meta
       mac-option-modifier 'none)
 
-;; Auto refresh buffers
-(global-auto-revert-mode 1)
-
-;; Also auto refresh dired, but be quiet about it
+;; Quietly auto refresh dired
 (setq global-auto-revert-non-file-buffers t)
 (setq auto-revert-verbose nil)
 
