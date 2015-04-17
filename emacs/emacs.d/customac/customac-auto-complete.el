ffffcb5f5a5d41f221e829556304e0c85086f346
Sheel Choksi <sheelchoksi@gmail.com>
2015-04-17 09:12:17 -0700

    Move clojure autocomplete to autocomplete file

--- emacs/emacs.d/customac/customac-auto-complete.el
+++ emacs/emacs.d/customac/customac-auto-complete.el
@@ -3,4 +3,8 @@
 (setq company-idle-delay 0)
 (setq company-minimum-prefix-length 1)
 
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)
+(add-hook 'clojure-mode-hook #'company-mode)
+
 (provide 'customac-auto-complete)
--- emacs/emacs.d/customac/customac-clojure.el
+++ emacs/emacs.d/customac/customac-clojure.el
@@ -54,10 +54,6 @@
 (add-hook 'clojure-mode-hook #'subword-mode)
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 
-(add-hook 'cider-repl-mode-hook #'company-mode)
-(add-hook 'cider-mode-hook #'company-mode)
-(add-hook 'clojure-mode-hook #'company-mode)
-
 (add-hook 'cider-repl-mode-hook #'smartparens-strict-mode)
 (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
 
