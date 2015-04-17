141ef46e88e4f989ab10d3bac2c480c91ef00d98
Sheel Choksi <sheelchoksi@gmail.com>
2015-04-16 22:46:27 -0700

    Smartparens strict mode/rainbow delimiters in repl

--- emacs/emacs.d/customac/customac-clojure.el
+++ emacs/emacs.d/customac/customac-clojure.el
@@ -58,4 +58,7 @@
 (add-hook 'cider-mode-hook #'company-mode)
 (add-hook 'clojure-mode-hook #'company-mode)
 
+(add-hook 'cider-repl-mode-hook #'smartparens-strict-mode)
+(add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
+
 (provide 'customac-clojure)
