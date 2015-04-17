989e4bfdfb7957600f485b54b5c4e310d60410bc
Sheel Choksi <sheelchoksi@gmail.com>
2015-04-16 22:37:16 -0700

    Switch from auto-complete-mode to company-mode

--- emacs/emacs.d/customac/customac-auto-complete.el
+++ emacs/emacs.d/customac/customac-auto-complete.el
@@ -1,30 +1,8 @@
-(customac-ensure-package 'auto-complete)
-(require 'auto-complete)
+(customac-ensure-package 'company)
 
-(ac-config-default)
+(global-company-mode)
 
-(global-auto-complete-mode t)
-(setq ac-auto-show-menu t)
-(setq ac-dwim t)
-(setq ac-use-menu-map t)
-(setq ac-quick-help-delay 1)
-(setq ac-quick-help-height 60)
-(setq ac-disable-inline t)
-(setq ac-show-menu-immediately-on-auto-complete t)
-(setq ac-auto-start 2)
-(setq ac-candidate-menu-min 0)
-
-(set-default 'ac-sources
-             '(ac-source-dictionary
-               ac-source-words-in-buffer
-               ac-source-words-in-same-mode-buffers
-               ac-source-semantic
-               ac-source-yasnippet))
-
-(dolist (mode '(magit-log-edit-mode log-edit-mode org-mode text-mode haml-mode
-                sass-mode yaml-mode csv-mode espresso-mode haskell-mode
-                html-mode nxml-mode sh-mode smarty-mode clojure-mode
-                lisp-mode textile-mode markdown-mode tuareg-mode))
-  (add-to-list 'ac-modes mode))
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 1)
 
 (provide 'customac-auto-complete)
