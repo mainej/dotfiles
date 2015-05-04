ed9b8811d83203df00138e5026b9038205705311
Sheel Choksi <sheelchoksi@gmail.com>
2015-05-03 23:10:48 -0700

    Add compojure indentation fixes

--- emacs/emacs.d/customac/customac-clojure.el
+++ emacs/emacs.d/customac/customac-clojure.el
@@ -61,4 +61,14 @@
 (add-hook 'cider-repl-mode-hook #'smartparens-strict-mode)
 (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
 
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
+
 (provide 'customac-clojure)
