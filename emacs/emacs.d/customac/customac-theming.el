e48f68a4fe66eed5fe2b3837c1f5dc02b0974a38
Alex Crough <alexc@mayvenn.com>
2015-08-28 16:33:00 -0700

    Set the font to PragmataPro if it exists.
    Seriously, Pragmata is the best programming font that has ever existed.

--- emacs/emacs.d/customac/customac-theming.el
+++ emacs/emacs.d/customac/customac-theming.el
@@ -4,4 +4,8 @@
 (load-theme 'sanityinc-tomorrow-night t)
 (set-face-attribute 'default nil :height 160)
 
+(let ((font "PragmataPro 18"))
+  (when (find-font (font-spec :name font))
+    (set-frame-font font)))
+
 (provide 'customac-theming)
